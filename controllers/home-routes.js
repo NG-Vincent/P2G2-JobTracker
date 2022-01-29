256e807f7142fed378a760a50e4708aade887443
Kelerik <vincent.ng39@gmail.com>
2022-01-29 16:16:56 -0500

    placeholder homepage setup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const {} = require("../models");
 
 // show all posts
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+   res.send("Hello World!");
+});
 
 module.exports = router;
