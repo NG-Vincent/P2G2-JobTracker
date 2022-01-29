f43c088afdc23827de6faeab9a778b3087fb2cde
Kelerik <vincent.ng39@gmail.com>
2022-01-29 16:11:33 -0500

    initial controllers code

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+const homeRoutes = require("./home-routes.js");
+const apiRoutes = require("./api");
+
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+
+router.use((req, res) => {
+   res.status(404).end();
+});
+
+module.exports = router;
