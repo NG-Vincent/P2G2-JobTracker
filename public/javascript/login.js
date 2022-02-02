93b93d51093acddbd394c2de2c5c14dbdf30e87b
Kelerik <vincent.ng39@gmail.com>
2022-02-02 18:27:00 -0500

    change login redirect

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -15,7 +15,7 @@ async function loginFormHandler(event) {
       });
 
       if (response.ok) {
-         document.location.replace("/kanban");
+         document.location.replace("/calendar");
       } else {
          alert(response.statusText);
       }
